0077dac2a1195780254142be4e495ccd9957634d
Joseph Patrick <jpatrick5402@gmail.com>
2025-09-28 20:46:36 -0400

    Adding header and layout of about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import Link from 'next/link';
 
 const jetbrains = JetBrains_Mono({
   subsets: ['latin'],
@@ -8,13 +9,18 @@ const jetbrains = JetBrains_Mono({
 
 export const metadata: Metadata = {
   title: "Joseph Patrick",
-  description: "An lifelong learner",
+  description: "A student forever and sometimes a teacher",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body className={`${jetbrains.className} antialiased`}>
+      <body className={`flex flex-col ${jetbrains.className} antialiased`}>
+        <nav className="flex bg-(--background) w-full h-10 gap-4 text-2xl font-bold items-center align-center justify-center">
+          <Link className="flex m-auto" href='/'>ABOUT</Link>
+          <Link className="flex m-auto" href='/projects'>PROJECTS</Link>
+          <Link className="flex m-auto" href='https://store.jp54.dev'>STORAGE</Link>
+        </nav>
         {children}
       </body>
     </html>
---
+++ app/projects/page.tsx
@@ -0,0 +1,11 @@
+import { FaHandSpock, FaLaptopCode } from "react-icons/fa";
+import { FiCoffee } from "react-icons/fi";
+import { IoIosMail } from "react-icons/io";
+import { IoShareSocial } from "react-icons/io5";
+
+export default function Home() {
+  return (
+    <div>
+    </div>
+  );
+}
