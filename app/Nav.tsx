3d3928d76874e5d4df4013cb740d9088f0217c9c
jpatrick5402 <jpatrick5402@gmail.com>
2025-09-30 16:01:58 -0400

    Basic 3d text working

--- app/Nav.tsx
+++ app/Nav.tsx
@@ -7,6 +7,8 @@ import { usePathname } from 'next/navigation';
 export default function Nav() {
   const [showNav, setShowNav] = useState(false);
 
+  const url = usePathname();
+  
   const urlMap: Record<string, string> = {
     '/': "/ABOUT",
     '/projects': "/PROJECTS",
@@ -14,24 +16,24 @@ export default function Nav() {
   }
 
   return (
-    <div>
-        <div className="flex sm:hidden bg-(--background) w-full pt-3 pb-3 gap-4 text-2xl font-bold items-center align-center justify-center">
+    <div className="flex w-full bg-(--background)">
+        <nav className="hidden sm:flex bg-(--background) max-w-5xl w-full m-auto pt-3 pb-3 gap-4 text-2xl font-bold items-center align-center justify-center">
+          <Link className={`flex m-auto ${url === '/' && 'text-(--text-accent)'}`} href='/'>ABOUT</Link>
+          <Link className={`flex m-auto ${url === '/projects' && 'text-(--text-accent)'}`} href='/projects'>PROJECTS</Link>
+          <Link className={`flex m-auto`} href='https://store.jp54.dev'>STORAGE<FaExternalLinkSquareAlt className="m-auto p-1" /></Link>
+          <Link className={`flex m-auto ${url === '/photography' && 'text-(--text-accent)'}`} href='/photography'>PHOTOGRAPHY</Link>
+        </nav>
+        <nav className="flex sm:hidden bg-(--background) m-auto max-w-5xl gap-4 pt-3 pb-3 text-2xl font-bold items-center align-center justify-center">
           <div className="" onClick={() => setShowNav(!showNav)}>
             <p className="text-center">MENU:</p>
-            <p className="text-center">{urlMap[usePathname()]}</p>
+            <p className="text-center">{urlMap[url]}</p>
           </div>
           {showNav && <div>
-            <Link className="flex m-auto" onClick={() => setShowNav(!showNav)} href='/'>ABOUT</Link>
-            <Link className="flex m-auto" onClick={() => setShowNav(!showNav)} href='/projects'>PROJECTS</Link>
-            <Link className="flex m-auto" onClick={() => setShowNav(!showNav)} href='https://store.jp54.dev'>STORAGE<FaExternalLinkSquareAlt className="m-auto p-1" /></Link>
-            <Link className="flex m-auto" onClick={() => setShowNav(!showNav)} href='/photography'>PHOTOGRAPHY</Link>
+            <Link className={`flex m-auto ${url === '/' && 'text-(--text-accent)'}`} onClick={() => setShowNav(!showNav)} href='/'>ABOUT</Link>
+            <Link className={`flex m-auto ${url === '/projects' && 'text-(--text-accent)'}`} onClick={() => setShowNav(!showNav)} href='/projects'>PROJECTS</Link>
+            <Link className={`flex m-auto`} onClick={() => setShowNav(!showNav)} href='https://store.jp54.dev'>STORAGE<FaExternalLinkSquareAlt className="m-auto p-1" /></Link>
+            <Link className={`flex m-auto ${url === '/photography' && 'text-(--text-accent)'}`} onClick={() => setShowNav(!showNav)} href='/photography'>PHOTOGRAPHY</Link>
           </div>}
-        </div>
-        <nav className="hidden sm:flex bg-(--background) w-full pt-3 pb-3 gap-4 text-2xl font-bold items-center align-center justify-center">
-          <Link className="flex m-auto" href='/'>ABOUT</Link>
-          <Link className="flex m-auto" href='/projects'>PROJECTS</Link>
-          <Link className="flex m-auto" href='https://store.jp54.dev'>STORAGE<FaExternalLinkSquareAlt className="m-auto p-1" /></Link>
-          <Link className="flex m-auto" href='/photography'>PHOTOGRAPHY</Link>
         </nav>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,36 @@
+'use client';
 import { FaHandSpock, FaLaptopCode, FaGithub, FaLinkedin } from "react-icons/fa";
 import { FiCoffee } from "react-icons/fi";
 import { IoIosMail } from "react-icons/io";
 import { IoShareSocial } from "react-icons/io5";
 import Image from 'next/image';
+
+import { extend } from '@react-three/fiber'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import almendra from '../Almendra SC_Regular.json'
+
+extend({ OrbitControls, TextGeometry })
+
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    textGeometry: Object3DNode<TextGeometry, typeof TextGeometry>;
+  }
+}
+
+function Text() {
+  const font = new FontLoader().parse(almendra);
+
+  return (
+    <mesh position={[-5,0,0]}>
+      <textGeometry args={['Joseph Patrick', {font, size: 1, height: 1, depth: .1}]} />
+      <meshPhysicalMaterial attach="material" color={'white'}/>
+    </mesh>
+  )
+}
+
 
 export default function Home() {
   return (
@@ -10,9 +38,11 @@ export default function Home() {
       <div className="flex flex-col m-auto max-w-5xl items-center justify-center">
         <div className="flex flex-col sm:flex-row p-2 m-auto items-center justify-center gap-4">
           <Image width='100' height='100' src='/profile.jpg' alt='Profile Pic' className="flex w-[15%] min-w-[180px] rounded-4xl border-5 border-black" />
-          <h1 className="p-2 text-2xl text-(--text-accent) flex bg-(--background) rounded-sm">Hello, My name is Joseph Patrick&nbsp;
-            <FaHandSpock className="flex mt-auto mb-auto" />
-          </h1>
+          <Canvas>
+            <OrbitControls></OrbitControls>
+            <ambientLight intensity={2}></ambientLight>
+            <Text></Text>
+          </Canvas>
         </div>
         <div className="flex flex-col gap-2 p-2">
           <div className="flex flex-col m-auto mb-6">
