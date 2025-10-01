4702a8cbb747eb0c045d43c960f086282eae9bce
jpatrick5402 <jpatrick5402@gmail.com>
2025-10-01 14:25:50 -0400

    Adding 3d laptop

---
+++ app/WebPage.tsx
@@ -0,0 +1,9 @@
+export default function Website() {
+  return (
+    <div>
+      <h1 className="text-center text-7xl text-black">Joseph</h1>
+      <h1 className="text-center text-7xl text-black">Patrick</h1>
+    </div>
+  )
+}
+
